cf0e79460bf9ebc7f73c7fa4cbfed4e2b053809f
sammy <tufysooy@gmail.com>
2025-11-07 16:06:03 +0800

    Update sermain.js

--- scripts/sermain.js
+++ scripts/sermain.js
@@ -3,7 +3,7 @@
 // Maintenance Time Config
 // ===============================
 const NEW_START = "2025-11-07T12:15:00";
-const NEW_END = "2025-11-08T7:30:00";
+const NEW_END = "2025-11-08T07:30:00";
 
 // ===============================
 // Local Storage Helpers
@@ -118,4 +118,5 @@ function updateTimeDisplays() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
 
